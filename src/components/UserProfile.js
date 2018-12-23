79e81686bb331f784d899eff579e9d16b27fe3a5
brewchetta <chett.tiller@gmail.com>
2018-12-23 17:04:35 -0500

    Refactoring user show to avoid state

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,52 +1,27 @@
 import React from 'react'
 import UserStoryCards from './UserStoryCards'
 
-export default class UserProfile extends React.Component {
+const UserProfile = (props) =>  {
 
-  // Define userId
-  userId = parseInt(this.props.match.params.userId)
+  // Define User
+  const userId = parseInt(props.match.params.userId)
+  console.log('UserID: ', userId)
 
-  //Define user state
-  state = {
-    user: {name: null}
-  }
+  const user = props.users.find(user => user.id === userId)
+  console.log('User: ', user)
 
-
-
-
-  componentDidMount() {
-    if (this.props.users.length === 0) {
-      this.setUserDelayed()
-    } else {
-      this.setState({ user: this.users.find(u => u.id === this.userId) })
-    }
-  }
-
-  setUser = () => {
-    this.setState({
-      user: this.props.users.find(u => u.id === this.userId)
-    })
-  }
-
-  setUserDelayed = () => {
-    if (!this.state.user || !this.state.user.name) {
-      setTimeout(this.setUser, 1000)
-      setTimeout(this.setUserDelayed, 1000)
-    }
-  }
-
-  render() {
-    console.log('User: ', this.state.user)
-    if (this.state.user && this.state.user.name) {
-      return (
-        <>
-        <h2>{this.state.user.name}</h2>
+  if (user) {
+    props.fetchUserStories(userId)
+    return (
+      <>
+        <h2>{user.name}</h2>
         <p>Wow! You've gotten to this page! Congratulations!</p>
-        <UserStoryCards userStories={this.props.userStories} />
-        </>
-      )
-    } else {
-      return (<p>Loading</p>)
-    }
+        <UserStoryCards userStories={props.userStories} />
+      </>
+    )
+  } else {
+    return (<p>Loading</p>)
   }
 }
+
+export default UserProfile
