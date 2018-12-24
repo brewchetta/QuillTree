ab77ffe42d42ba3713dd742f0d704d81ec8feb2d
brewchetta <chett.tiller@gmail.com>
2018-12-24 10:24:53 -0500

    Added search bar for users

--- src/components/User/UserIndex.js
+++ src/components/User/UserIndex.js
@@ -1,24 +1,44 @@
 import React from 'react'
 import UserCard from './UserCard'
 
-const UserIndex = (props) => {
+class UserIndex extends React.Component {
+
+  state = {
+    searchInput: ''
+  }
 
   // Renders all users
-  const renderUsers = () => {
-    if (props.users) {
-      return props.users.map(user => {
-        return <UserCard key={user.name} user={user} />
+  renderUsers = () => {
+    if (this.props.users) {
+      return this.props.users.map(user => {
+        return user.name.toLowerCase().includes(this.state.searchInput.toLowerCase()) ?
+        <UserCard key={user.name} user={user} />
+        : null
       })
     }
   }
 
+  handleInput = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
   // Main render
-  return (
-    <div>
-    <h2>USER LIST</h2>
-    {renderUsers()}
-    </div>
-  )
+  render() {
+    return (
+      <div>
+      <input type='text'
+      name='searchInput'
+      placeholder='Search Users'
+      value={this.state.searchInput}
+      onChange={this.handleInput} />
+      <h2>USER LIST</h2>
+      {this.renderUsers()}
+      </div>
+    )
+  }
+
 }
 
 export default UserIndex
