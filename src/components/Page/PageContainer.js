ea31381c7d0be045367ca0655eb24fe70b41bade
brewchetta <chett.tiller@gmail.com>
2018-12-27 11:11:49 -0500

    Added field for editing page content

--- src/components/Page/PageContainer.js
+++ src/components/Page/PageContainer.js
@@ -4,7 +4,8 @@ export default class PageContainer extends React.Component {
 
   state = {
     page: {},
-    edit: false
+    edit: false,
+    content: ''
   }
 
   // Define User and Story
@@ -17,7 +18,7 @@ export default class PageContainer extends React.Component {
     if (this.state.page.id && this.state.page.id === parseInt(this.props.match.params.pageId)) {
     } else if (this.story && this.user) {
       this.props.fetchPage(this.user.id, this.story.id, this.props.match.params.pageId)
-      .then(page => this.setState({ page: page }))
+      .then(page => this.setState({ page: page, content: page.content }))
     }
   }
 
@@ -29,6 +30,10 @@ export default class PageContainer extends React.Component {
     this.setState({ edit: false })
   }
 
+  handleChange = (event) => {
+    this.setState({ content: event.target.value })
+  }
+
   // Main render
   render() {
     // Redefine user and story
@@ -37,16 +42,14 @@ export default class PageContainer extends React.Component {
     this.handleFetchPage()
     return (
       <div>
-
         {this.user ? <p>{this.user.name}</p> : <p>Loading</p> }
         {this.story ? <p>{this.story.title}</p> : <p>Loading</p> }
         <p>Page #{this.state.page.number}</p>
         <p>Image: {this.state.page.image}</p>
 
-        { this.state.edit ? <p>Edit Mode</p> : <p>Content: {this.state.page.content}</p> }
+        { this.state.edit ? <textarea value={this.state.content} onChange={this.handleChange}/> : <p>Content: {this.state.page.content}</p> }
 
         { this.state.edit ? <button onClick={this.handleClickSave}>Save</button> : <button onClick={this.handleClickEdit}>Edit</button> }
-
       </div>
     )
   }
