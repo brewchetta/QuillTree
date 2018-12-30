deefa66f9d4be8734e1ade0a1c9af4edaadad0c1
brewchetta <chett.tiller@gmail.com>
2018-12-30 18:16:54 -0500

    Page numbers are properly sorted in the page select dropdown

--- src/components/Story/StoryPageOptions.js
+++ src/components/Story/StoryPageOptions.js
@@ -4,7 +4,7 @@ const StoryPageOptions = (props) => {
 
   const renderPageOptions = () => {
     if (props.pages) {
-      return props.pages.map(page => {
+      return props.pages.sort((a,b) => a.number - b.number).map(page => {
         return (
           <option key={page.id} value={page.number}>{page.number}</option>
         )
