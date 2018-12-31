a106c2728d2af0aaac7a7043653b1c357181588b
brewchetta <chett.tiller@gmail.com>
2018-12-30 19:09:53 -0500

    Added popup for login

--- src/components/LoginBar.js
+++ src/components/LoginBar.js
@@ -2,24 +2,49 @@ import React from 'react'
 
 export default class LoginBar extends React.Component {
 
+  // Set current users and such for ease of access
   currentUser = this.props.currentUser
   users = this.props.users
 
   state = {
-    loginPopup: false
+    loginPopup: false,
+    name: ''
+  }
+
+  // For controlled form
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  // Determines whether the login popup is needed and then renders it
+  renderPopup = () => {
+    if (!this.state.loginPopup) {
+      return (
+        <form className='login-popup'>
+          <label>Username: </label>
+          <input type='text'
+          name='name'
+          value={this.state.name}
+          onChange={this.handleChange}/>
+          <button>Log In</button>
+
+        </form>
+      )
+    }
   }
 
   render() {
     if (this.currentUser) {
       return (
         <div className='navbar-right'>
-        <p>I AM LINK TO USER PROFILE</p>
+          <p>I AM LINK TO USER PROFILE</p>
         </div>
       )
     } else {
       return (
         <div className='navbar-right'>
-        <p>I AM LINK LOGIN / SIGNUP</p>
+          <p>I AM LINK LOGIN / SIGNUP</p>
+          {this.renderPopup()}
         </div>
       )
     }
