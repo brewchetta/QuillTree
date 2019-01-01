83216c19c9835a089bfa5e280d482ccac89aeadf
brewchetta <chett.tiller@gmail.com>
2018-12-31 19:37:40 -0500

    Added fonts

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import QuillTreeImage from '../assets/quill-tree-sil.png'
 
 const Home = (props) => {
 
   return (
     <div id='home-main'>
-      <h1 className='home-title'>QuillTree</h1>
+      <h1 className='home-title'><img alt='I am tree' src={QuillTreeImage} /> QuillTree</h1>
       <div className='home-tile-container'>
         <p className='home-info'>Welcome to QuillTree, the premier storytelling medium on the internet!<br/> You can sign up 'here!' or log in 'here'!</p>
       </div>
